6915d710a0d621b3776e1f836d71d46737383c36
jdvgsa <johnr.dagsa@outlook.com>
2023-11-08 23:51:08 -0700

    test

---
+++ src/lib/data.js
@@ -0,0 +1,21 @@
+export const cardData = {
+  myTitle: "Tea",
+  myDescription: "Best Drank Warm",
+  myImage: "../images/tea.jpg",
+  myLink: "https://www.google.ca"
+};
+
+export const cardArray = [
+  {myTitle: "This is Tea.",
+  myDescription: "You should always drink it warm.",
+  myImage: "../images/tea.jpg",
+  myLink: "https://www.google.ca",},
+  {myTitle:"This is Tea.",
+  myDescription: "You should always drink it warm.",
+  myImage: "../images/tea.jpg",
+  myLink: "https://www.google.ca",},
+  {myTitle:"This is Tea.",
+  myDescription: "You should always drink it warm.",
+  myImage: "../images/tea.jpg",
+  myLink: "https://www.google.ca",}
+]
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script> 
+import Card from "../lib/components/Card.svelte";
+import {cardData} from "../lib/data.js"
+import {cardArray} from "../lib/data.js"
+// renders card
+let visible = true;
+</script>
+<head>
+<style>
+nav {
+  
+  background-color: aquamarine;
+  padding: 0.5rem;
+}
+
+nav a {
+  color: grey;
+  text-decoration: none;
+  margin-right: 1rem;
+}
+
+nav a:hover {
+  text-decoration:underline;
+  color: red;
+
+}
+</style>
+</head>
+
+   <nav>
+    
+      <li> <a href="/">Home</a></li>
+      <li> <a href="/about">About</a></li>
+      <li> <a href="/contact">Contact</a></li>
+    
+  </nav>
+  
+
+<h1 class="text-5xl font-bold underline">Components and Props Assignment Test3322</h1>
+
+<div>
+<Card {...cardData}/>
+</div>
+
+<h1>Looping cardArray</h1>
+<span>
+
+{#each cardArray as card}
+<Card {...card}/>
+{/each}
+
+</span>
+
+<footer><p>&copy; 2023 John Dagsa Svelte Assignment</p></footer>
+
